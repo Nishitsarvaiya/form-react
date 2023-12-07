1e2a6b4f3c72cc070048f5288b99676eafb1e052
Nishit Sarvaiya <nishit@darwindigital.com>
2023-12-07 15:55:11 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -14,35 +14,88 @@ function App() {
 		age: "",
 	});
 
+	const [tableData, setTableData] = useState({
+		full_name: "",
+		email: "",
+		phone: "",
+		company: "",
+		age: "",
+	});
+
 	const [isEditable, setIsEditable] = useState(false);
+
+	const inputChangeHandler = (e) => {
+		setData({ ...data, [e.target.name]: e.target.value });
+	};
 
 	const formSubmitHandler = (e) => {
 		e.preventDefault();
+		setTableData({ ...data });
 	};
+
 	return (
 		<main>
 			<div className="min-h-screen grid grid-cols-1 lg:grid-cols-2 divide-y lg:divide-x">
 				<div className="grid items-center px-6 py-10 lg:px-[10vw]">
 					<form className="w-full flex flex-col" onSubmit={formSubmitHandler}>
-						<Input type="text" placeholder="Full Name" name="" className="text-md h-12 px-4 mb-2" />
-						<Input type="email" placeholder="Email" name="" className="text-md h-12 px-4 mb-2" />
-						<Input type="text" placeholder="Phone" name="" className="text-md h-12 px-4 mb-2" />
-						<Input type="text" placeholder="Company" name="" className="text-md h-12 px-4 mb-2" />
-						<Input type="text" placeholder="Age" name="" className="text-md h-12 px-4 mb-4" />
-						<div className="flex items-center space-x-2 mb-4">
-							<Checkbox
-								className="rounded-none"
-								id="isEditable"
-								checked={isEditable}
-								onCheckedChange={() => setIsEditable(!isEditable)}
-							/>
-							<label
-								htmlFor="isEditable"
-								className="text-md leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-								Editable ?
-							</label>
+						<Input
+							type="text"
+							placeholder="Full Name"
+							name="full_name"
+							id="full_name"
+							className="text-md h-12 px-4 mb-2"
+							onChange={inputChangeHandler}
+						/>
+						<Input
+							type="email"
+							placeholder="Email"
+							name="email"
+							id="email"
+							className="text-md h-12 px-4 mb-2"
+							onChange={inputChangeHandler}
+						/>
+						<Input
+							type="text"
+							placeholder="Phone"
+							name="phone"
+							id="phone"
+							className="text-md h-12 px-4 mb-2"
+							onChange={inputChangeHandler}
+						/>
+						<Input
+							type="text"
+							placeholder="Company"
+							name="company"
+							id="company"
+							className="text-md h-12 px-4 mb-2"
+							onChange={inputChangeHandler}
+						/>
+						<Input
+							type="text"
+							placeholder="Age"
+							name="age"
+							id="age"
+							className="text-md h-12 px-4 mb-4"
+							onChange={inputChangeHandler}
+						/>
+						<div className="flex justify-between items-start mb-4">
+							<div className="flex items-center space-x-2">
+								<Checkbox
+									className="rounded-none"
+									id="isEditable"
+									checked={isEditable}
+									onCheckedChange={() => setIsEditable(!isEditable)}
+								/>
+								<label
+									htmlFor="isEditable"
+									className="text-md leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+									Editable ?
+								</label>
+							</div>
+							<Button className="text-md h-12 px-6 self-end" type="submit">
+								Submit
+							</Button>
 						</div>
-						<Button className="text-md h-12 px-6 self-end">Submit</Button>
 					</form>
 				</div>
 				<div className="grid items-center px-6 py-10 lg:px-[10vw]">
@@ -50,7 +103,7 @@ function App() {
 						<TableBody>
 							<TableRow>
 								<TableHead>Full Name</TableHead>
-								<TableCell>{data.full_name}</TableCell>
+								<TableCell>{tableData.full_name}</TableCell>
 								<TableCell className="text-right">
 									{isEditable && (
 										<Button variant="icon">
@@ -64,7 +117,7 @@ function App() {
 							</TableRow>
 							<TableRow>
 								<TableHead>Email</TableHead>
-								<TableCell>{data.email}</TableCell>
+								<TableCell>{tableData.email}</TableCell>
 								<TableCell className="text-right">
 									{isEditable && (
 										<Button variant="icon">
@@ -78,7 +131,7 @@ function App() {
 							</TableRow>
 							<TableRow>
 								<TableHead>Phone</TableHead>
-								<TableCell>{data.phone}</TableCell>
+								<TableCell>{tableData.phone}</TableCell>
 								<TableCell className="text-right">
 									{isEditable && (
 										<Button variant="icon">
@@ -92,7 +145,7 @@ function App() {
 							</TableRow>
 							<TableRow>
 								<TableHead>Company</TableHead>
-								<TableCell>{data.company}</TableCell>
+								<TableCell>{tableData.company}</TableCell>
 								<TableCell className="text-right">
 									{isEditable && (
 										<Button variant="icon">
@@ -106,7 +159,7 @@ function App() {
 							</TableRow>
 							<TableRow>
 								<TableHead>Age</TableHead>
-								<TableCell>{data.age}</TableCell>
+								<TableCell>{tableData.age}</TableCell>
 								<TableCell className="text-right">
 									{isEditable && (
 										<Button variant="icon">
